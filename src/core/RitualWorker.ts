49c1892a081fc0287a96b341f03a48f50b67d834
agorgulu <ahmetnecipg@gmail.com>
2025-01-22 17:48:48 +0300

    Add log

--- src/core/RitualWorker.ts
+++ src/core/RitualWorker.ts
@@ -671,6 +671,7 @@ export class RitualWorker {
           );
         }
 
+        console.log("TRIGGER voting-result with winners:", remainingPlayers.map((p) => p.wallet_address) );
         await this.pusher.trigger(`lobby-${lobby.id}`, "voting-result", {
           result: "share",
           winners: remainingPlayers.map((p) => p.wallet_address),
